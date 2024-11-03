f7b745037e2e67fc99a5619e2537c117fde94d3b
jefrin <jefrinlaia123@gmail.com>
2024-11-03 21:52:27 +0700

    desain invoice backend

--- resources/views/components/backend/invoice.blade.php
+++ resources/views/components/backend/invoice.blade.php
@@ -1,22 +1,126 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="zxx">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Invoice</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+        }
+
+        h1, h3, h6 {
+            margin: 0;
+        }
+
+        .container {
+            width: 100%;
+        }
+
+        .table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+
+        .table th, .table td {
+            border: 1px solid #000;
+            padding: 10px;
+            text-align: left;
+        }
+
+        .text-end {
+            text-align: end;
+        }
+    </style>
 </head>
+
 <body>
-    <h1>Invoice Konfirmasi Pesanan</h1>
-    <p>Nama Customer: {{ $customer_name }}</p>
-    <p>Total Harga: Rp. {{ number_format($total_price, 2) }}</p>
+    <div class="container">
+        <div class="row">
+            <div class="col mt-5">
+                <div class="row">
+                    <div class="col">
+                        <div class="single_blog">
+                            <div class="blog_content">
+                                <div class="row">
+                                    <div class="col">
+                                        <img src="assets_frontend/img/logo/logo.png" alt="">
+                                    </div>
+                                    <div class="col text-end">
+                                        <div style="display: flex; justify-content: space-between; align-items: center;">
+                                            <h1>INVOICE</h1>
+                                            <h1>Kupon No. {{ $kupon_pesanan }}</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row mt-5">
+                                    <div class="col-6">
+                                        <h3 class="mb-3">BILLED TO :</h3>
+                                         <h6>{{ $customer_name }}
+                                                <br> {{ $tanggal_pembelian }}
+                                                <br> MYC Dorm, E307
+                                            </h6>
+                                    </div>
+                                </div>
 
-    <h3>Detail Pesanan:</h3>
-    <ul>
-        @foreach ($pesanan as $detail)
-            <li>{{ $detail }}</li>
-        @endforeach
-    </ul>
-
-    <h3>NO.Kupon : {{ $kupon_pesanan }}</h3>
+                                <div class="mt-5">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                               <th class="cart-product-name">Tanggal Pengambilan</th>
+                                                <th class="cart-product-name">Nama Makanan</th>
+                                                <th class="cart-product-name">Jenis Paket</th>
+                                                <th class="cart-product-name">Kategori</th>
+                                                <th class="cart-product-total">harga</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                             @foreach ( $pesanan as $detail )
+                                                            
+                                                        <tr>
+                                                            <td class="cart-product-name">{{ $detail['tanggal_pengambilan'] }}</td>
+                                                            <td class="cart-product-name">{{ $detail['nama_pesanan'] }}</td>
+                                                            <td class="cart-product-name"> {{ $detail['jenis_paket'] }}</td>
+                                                            <td class="cart-product-name">{{ $detail['kategori'] }} </td>
+                                                            <td class="cart-product-total">{{ $detail['harga'] }}</td>
+                                                        </tr>
+                                                        
+                                                        @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                            
+                                            <tr>
+                                                <th>Order Total</th>
+                                                <td colspan="3"></td>
+                                                <td>{{ $harga_total }}</td>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <div class="row mt-5">
+                                        <div class="col">
+                                            <h1>Thank You!</h1>
+                                        </div>
+                                    </div>
+                                    <div class="row mt-5">
+                                        <div class="col-6">
+                                            <h3 class="mb-3">PAYMENT INFORMATION :</h3>
+                                            <h6>TRANSFER BANK<br>Account Name: Catherine Lauren<br>Date: 26 October 2024</h6>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
---
+++ resources/views/components/backend/invoice3.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    {{-- <h1>Invoice Konfirmasi Pesanan</h1>
+    <p>Nama Customer: {{ $customer_name }}</p>
+    <p>Total Harga: Rp. {{ number_format($total_price, 2) }}</p>
+
+    <h3>Detail Pesanan:</h3>
+    <ul>
+        @foreach ($pesanan as $detail)
+            <li>{{ $detail }}</li>
+        @endforeach
+    </ul>
+
+    <h3>NO.Kupon : {{ $kupon_pesanan }}</h3> --}}
+    
+</body>
+</html>
