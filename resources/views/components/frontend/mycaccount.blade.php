6692f2ee8b384c9bd79d795daa59598a5fc2816d
bleuEve <cathlauren09@gmail.com>
2024-11-07 13:47:22 +0700

    update button logout

--- resources/views/components/frontend/mycaccount.blade.php
+++ resources/views/components/frontend/mycaccount.blade.php
@@ -109,12 +109,16 @@
                         </li>
 
                         <li class="nav-item">
-                            <i class="icofont-logout"></i>
-                            <form action="/logout" method="POST">
-                                @csrf
-                            <a class="nav-link" id="account-logout-tab" role="tab"
-                                aria-selected="false"><button type="submit">Logout</button>  <i class="icofont-logout"></i></a>
-                            </form>
+                        <div class="form_input_btn mt-3">
+                            <button type="submit" class="btn btn-link">Logout  
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right ms-2" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+                                <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"/>
+                                </svg>
+                            </button>
+                            
+                            
+                        </div>
                         </li>
                     </ul>
                 </div>
@@ -437,7 +441,7 @@
                                                                     <div class="modal-footer">
                                                                         <button type="button" class="btn custom-btn md-size"
                                                                             data-bs-dismiss="modal" data-bs-target="#login"
-                                                                            data-bs-toggle="modal">Back to</button>
+                                                                            data-bs-toggle="modal">Back to Login</button>
                                                                         <button type="submit"
                                                                             class="btn custom-btn md-size">Register</button>
                                                                     </div>
