d788234a7b48c7e896fb88d0b41e35f0cb94f72f
RyanJnthn <ryanjonathaneps@gmail.com>
2024-11-19 14:38:13 +0700

    jona

--- resources/views/components/backend/index.blade.php
+++ resources/views/components/backend/index.blade.php
@@ -62,7 +62,7 @@
                             <div class="row">
                                 <div class="col-lg-12">
                                     <div class="row">
-                                        <div class="col-lg-3">
+                                        <div class="col-lg-4">
                                             <div class="card">
                                                 <div class="card-body">
                                                     <div class="icon-contain">
@@ -93,7 +93,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-lg-3">
+                                        <div class="col-lg-4">
                                             <div class="card">
                                                 <div
                                                     class="card-body justify-content-center"
@@ -126,7 +126,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-lg-3">
+                                        <div class="col-lg-4">
                                             <div class="card">
                                                 <div class="card-body">
                                                     <div class="icon-contain">
@@ -157,41 +157,9 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-lg-3">
-                                            <div class="card">
-                                                <div class="card-body">
-                                                    <div class="icon-contain">
-                                                        <div class="row">
-                                                            <div
-                                                                class="col-2 align-self-center"
-                                                            >
-                                                                <i
-                                                                    class="fas fa-database text-gradient-primary"
-                                                                ></i>
-                                                            </div>
-                                                            <div
-                                                                class="col-10 text-right"
-                                                            >
-                                                                <h5
-                                                                    class="mt-0 mb-1"
-                                                                >
-                                                                    $15562
-                                                                </h5>
-                                                                <p
-                                                                    class="mb-0 font-12 text-muted"
-                                                                >
-                                                                    Budget
-                                                                </p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
                                     </div>
-                                    
-
-
+                                </div>
+                            </div>   
                             <div class="row">
                                 <div class="col-xl-12">
                                     <div class="card">
@@ -204,160 +172,60 @@
                                                     class="table table-hover mb-0"
                                                 >
                                                     <thead>
-                                                        <tr
-                                                            class="align-self-center"
-                                                        >
-                                                            <th>
-                                                                Project Name
-                                                            </th>
-                                                            <th>Client Name</th>
-                                                            <th>
-                                                                Payment Type
-                                                            </th>
-                                                            <th>Paid Date</th>
-                                                            <th>Amount</th>
-                                                            <th>Transaction</th>
+                                                        <tr>
+                                                            <th rowspan="2"  style="text-align: center; vertical-align: middle;">Asrama</th>
+                                                            <th rowspan="2"  style="text-align: center; vertical-align: middle;">Sesi</th>
+                                                            <th colspan="3"  style="text-align: center; vertical-align: middle;">SILVER MENU</th>
+                                                            <th colspan="3"  style="text-align: center; vertical-align: middle;">GOLD MENU</th>
+                                                        </tr>
+                                                        <tr style="text-align: center; vertical-align: middle;">
+                                                            <th>Harga Jual Makanan</th>
+                                                            <th>Modal</th>
+                                                            <th>Keuntungan</th>
+                                                            <th>Harga Jual Makanan</th>
+                                                            <th>Modal</th>
+                                                            <th>Keuntungan</th>
                                                         </tr>
                                                     </thead>
-                                                    <tbody>
+                                                    <tbody style="text-align: center; vertical-align: middle;">
                                                         <tr>
-                                                            <td>
-                                                                Product
-                                                                Devlopment
+                                                            <td rowspan="2">
+                                                                Scholarship
                                                             </td>
                                                             <td>
-                                                                <img
-                                                                    src="assets_backend/images/users/avatar-1.jpg"
-                                                                    alt=""
-                                                                    class="thumb-sm rounded-circle mr-2"
-                                                                />
-                                                                Kevin Heal
+                                                                L
                                                             </td>
-                                                            <td>Paypal</td>
-                                                            <td>5/8/2018</td>
-                                                            <td>$15,000</td>
-                                                            <td>
-                                                                <span
-                                                                    class="badge badge-boxed badge-soft-warning"
-                                                                    >panding</span
-                                                                >
-                                                            </td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
                                                         </tr>
                                                         <tr>
                                                             <td>
-                                                                New Office
-                                                                Building
+                                                                D
                                                             </td>
-                                                            <td>
-                                                                <img
-                                                                    src="assets_backend/images/users/avatar-2.jpg"
-                                                                    alt=""
-                                                                    class="thumb-sm rounded-circle mr-2"
-                                                                />
-                                                                Frank M. Lyons
-                                                            </td>
-                                                            <td>Paypal</td>
-                                                            <td>15/7/2018</td>
-                                                            <td>$35,000</td>
-                                                            <td>
-                                                                <span
-                                                                    class="badge badge-boxed badge-soft-primary"
-                                                                    >Success</span
-                                                                >
-                                                            </td>
-                                                        </tr>
-
-                                                        <tr>
-                                                            <td>
-                                                                Market Research
-                                                            </td>
-                                                            <td>
-                                                                <img
-                                                                    src="assets_backend/images/users/avatar-3.jpg"
-                                                                    alt=""
-                                                                    class="thumb-sm rounded-circle mr-2"
-                                                                />
-                                                                Angelo Butler
-                                                            </td>
-                                                            <td>Pioneer</td>
-                                                            <td>30/9/2018</td>
-                                                            <td>$45,000</td>
-                                                            <td>
-                                                                <span
-                                                                    class="badge badge-boxed badge-soft-warning"
-                                                                    >Panding</span
-                                                                >
-                                                            </td>
-                                                        </tr>
-
-                                                        <tr>
-                                                            <td>
-                                                                Website &amp;
-                                                                Blog
-                                                            </td>
-                                                            <td>
-                                                                <img
-                                                                    src="assets_backend/images/users/avatar-4.jpg"
-                                                                    alt=""
-                                                                    class="thumb-sm rounded-circle mr-2"
-                                                                />
-                                                                Phillip Morse
-                                                            </td>
-                                                            <td>Paypal</td>
-                                                            <td>2/6/2018</td>
-                                                            <td>$70,000</td>
-                                                            <td>
-                                                                <span
-                                                                    class="badge badge-boxed badge-soft-warning"
-                                                                    >Success</span
-                                                                >
-                                                            </td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
                                                         </tr>
                                                         <tr>
-                                                            <td>
-                                                                Product
-                                                                Devlopment
+                                                            <td rowspan="2">
+                                                                Paying
                                                             </td>
                                                             <td>
-                                                                <img
-                                                                    src="assets_backend/images/users/avatar-5.jpg"
-                                                                    alt=""
-                                                                    class="thumb-sm rounded-circle mr-2"
-                                                                />
-                                                                Kevin Heal
+                                                                L & D
                                                             </td>
-                                                            <td>Paypal</td>
-                                                            <td>5/8/2018</td>
-                                                            <td>$15,000</td>
-                                                            <td>
-                                                                <span
-                                                                    class="badge badge-boxed badge-soft-primary"
-                                                                    >panding</span
-                                                                >
-                                                            </td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>
-                                                                New Office
-                                                                Building
-                                                            </td>
-                                                            <td>
-                                                                <img
-                                                                    src="assets_backend/images/users/avatar-6.jpg"
-                                                                    alt=""
-                                                                    class="thumb-sm rounded-circle mr-2"
-                                                                />
-                                                                Frank M. Lyons
-                                                            </td>
-                                                            <td>Paypal</td>
-                                                            <td>15/7/2018</td>
-                                                            <td>$35,000</td>
-                                                            <td>
-                                                                <span
-                                                                    class="badge badge-boxed badge-soft-primary"
-                                                                    >Success</span
-                                                                >
-                                                            </td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
+                                                            <td>100</td>
                                                         </tr>
                                                     </tbody>
                                                 </table>
