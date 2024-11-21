1b03bab280cf84d010a582c603d9d3ce2b3bfb21
bleuEve <cathlauren09@gmail.com>
2024-11-21 09:53:12 +0700

    edit history controller

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -9,11 +9,7 @@ class HistoryController extends Controller
 {
     public function index()
 {
-    $user = auth()->user();
-
-    if (!$user) {
-        return redirect()->route('login')->with('error', 'Please log in to access your account.');
-    }
+    $user = auth()->user()->id;
 
     $history = DB::table('users')
         ->join('orders', 'users.id', '=', 'orders.user_id')
