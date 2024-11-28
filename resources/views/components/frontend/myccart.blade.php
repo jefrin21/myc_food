3a355fed1a4b555c340c4c4bb410cea57f09a05a
jefrin <jefrinlaia123@gmail.com>
2024-11-28 14:28:16 +0700

    data weeklys

--- resources/views/components/frontend/myccart.blade.php
+++ resources/views/components/frontend/myccart.blade.php
@@ -131,10 +131,10 @@
                                 <div class="country-select clearfix">
                                     <select class="myniceselect nice-select wide" name="jenispaket1" id="jenispaket1" >
                                         <option value="" disabled selected hidden>Pilih paket</option> <!-- Placeholder option -->
-                                        <option value="Silver">Silver A</option>
-                                        <option value="Silver">Silver B</option>
-                                        <option value="Gold">Gold A</option>
-                                        <option value="Gold">Gold B</option>
+                                        <option value="Silver A">Silver A</option>
+                                        <option value="Silver B">Silver B</option>
+                                        <option value="Gold A">Gold A</option>
+                                        <option value="Gold B">Gold B</option>
                                     </select>
                                 </div>
                             </div>
@@ -145,10 +145,10 @@
                                 <div class="country-select clearfix">
                                     <select class="myniceselect nice-select wide" name="jenispaket2" id="jenispaket2" >
                                         <option value="" disabled selected hidden>Pilih paket</option> <!-- Placeholder option -->
-                                        <option value="Silver">Silver A</option>
-                                        <option value="Silver">Silver B</option>
-                                        <option value="Gold">Gold A</option>
-                                        <option value="Gold">Gold B</option>
+                                        <option value="Silver A">Silver A</option>
+                                        <option value="Silver B">Silver B</option>
+                                        <option value="Gold A">Gold A</option>
+                                        <option value="Gold B">Gold B</option>
                                     </select>
                                 </div>
                                 
